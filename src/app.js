fa7e2ab059e7a749882057cb8852fb33661c109f
Lauren Auchter <lsauchter@gmail.com>
2019-08-22 10:28:00 -0400

    Fixed typo

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors({
-    origin: 'https://lsauchter-noteful.now.sh/'
+    origin: 'https://lsauchter-noteful.now.sh'
 }))
 
 app.use('/api/folders', foldersRouter)
